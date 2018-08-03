f136bf3109b9df9c349f38cbe1a2a24913ef2160
Meredith Reece <mardebugger@gmail.com>
2018-08-03 22:07:32 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
 
     if params[:artist_id]
-      @artist = Artist.find(params[:artist_id])
+      @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path
       else
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find(params[:artist_id])
+      @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find(params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist)
