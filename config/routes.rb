ce81d3520f5944e81f7a71be9c41450764d8db1d
Meredith Reece <mardebugger@gmail.com>
2018-08-03 21:30:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :show] do
+  resources :artists, only: [:show] do
     resources :songs, only: [:index, :show]
   end
   resources :songs
