a0e1b37fd03c7c51f93104af586af28fc955bfd0
Meredith Reece <mardebugger@gmail.com>
2018-08-03 22:07:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,7 +21,7 @@ class SongsController < ApplicationController
         redirect_to artist_songs_path(@artist)
       end
     else
-      @song = Song.find(params[:id])
+      @song = Song.find_by(id: params[:id])
     end
   end
 
